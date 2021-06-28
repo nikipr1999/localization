80c603e6db5e9dd8a9f420725864e8647579f92f
nikipr1999 <nikiprajapati1999@gmail.com>
2021-06-28 14:39:03 +0530

    Project and import page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './MyComponents/login/login.component';
 import { ProjectsComponent } from './MyComponents/projects/projects.component';
+import { ProjectImportComponent } from './MyComponents/project-import/project-import.component';
+import { ProjectListComponent } from './MyComponents/project-list/project-list.component';
 
 const routes: Routes = [
   {path:'', component: LoginComponent},
-  {path:'projects',component: ProjectsComponent},
+  {path:'projects',component: ProjectListComponent},
+  {path:'projects/import',component: ProjectImportComponent},
 ];
 
 @NgModule({
